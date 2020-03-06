bfd165acc062713c1d1a060b545665341aa5133a
root <root@SGHZ001032369.szh.apac.bosch.com>
2020-03-06 17:54:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿let express = require("express");
+let app = express();
+
+app.use(express.static("dist"));
+
+var server = app.listen(8083, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server Start! "+host+":"+port);
+})
